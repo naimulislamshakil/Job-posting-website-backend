df41d992a98ab1097e8ad448a700cb00a6a25c27
Naimul Islam <ms0413354@gmail.com>
2024-03-18 22:37:11 +0600

    hash password

--- Schema/singup.schema.js
+++ Schema/singup.schema.js
@@ -1,105 +1,118 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const singupSchema = mongoose.Schema({
-	First_Name: {
-		type: String,
-		required: true,
-		trim: true,
+const singupSchema = mongoose.Schema(
+	{
+		First_Name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		Last_Name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		Email: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		Position: {
+			type: String,
+		},
+		Country_Code: {
+			type: String,
+		},
+		Password: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		Number: {
+			type: String,
+		},
+		Company_Name: {
+			type: String,
+		},
+		Industry: {
+			type: String,
+		},
+		Employees: {
+			type: String,
+		},
+		EmployerType: {
+			type: String,
+		},
+		Website: {
+			type: String,
+		},
+		HearUs: {
+			type: String,
+		},
+		Contact_Person: {
+			type: String,
+		},
+		Notification_Email: {
+			type: String,
+		},
+		Cuntry: {
+			type: String,
+		},
+		Address: {
+			type: String,
+		},
+		Phone_Number: {
+			type: String,
+		},
+		CountryCode: {
+			type: String,
+		},
+		Days: {
+			type: String,
+		},
+		Months: {
+			type: String,
+		},
+		Years: {
+			type: String,
+		},
+		Gender: {
+			type: String,
+		},
+		Nationality: {
+			type: String,
+		},
+		Location: {
+			type: String,
+		},
+		Qualification: {
+			type: String,
+		},
+		Experience: {
+			type: String,
+		},
+		Job_Function: {
+			type: String,
+		},
+		Availability: {
+			type: String,
+		},
+		Desired_Job_Function: [],
+		Resume: {
+			type: String,
+		},
 	},
-	Last_Name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	Email: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	Position: {
-		type: String,
-	},
-	Country_Code: {
-		type: String,
-	},
-	Password: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	Number: {
-		type: String,
-	},
-	Company_Name: {
-		type: String,
-	},
-	Industry: {
-		type: String,
-	},
-	Employees: {
-		type: String,
-	},
-	EmployerType: {
-		type: String,
-	},
-	Website: {
-		type: String,
-	},
-	HearUs: {
-		type: String,
-	},
-	Contact_Person: {
-		type: String,
-	},
-	Notification_Email: {
-		type: String,
-	},
-	Cuntry: {
-		type: String,
-	},
-	Address: {
-		type: String,
-	},
-	Phone_Number: {
-		type: String,
-	},
-	CountryCode: {
-		type: String,
-	},
-	Days: {
-		type: String,
-	},
-	Months: {
-		type: String,
-	},
-	Years: {
-		type: String,
-	},
-	Gender: {
-		type: String,
-	},
-	Nationality: {
-		type: String,
-	},
-	Location: {
-		type: String,
-	},
-	Qualification: {
-		type: String,
-	},
-	Experience: {
-		type: String,
-	},
-	Job_Function: {
-		type: String,
-	},
-	Availability: {
-		type: String,
-	},
-	Desired_Job_Function: [],
-	Resume: {
-		type: String,
-	},
+	{ timestamps: true }
+);
+
+singupSchema.pre('save', function (next) {
+	if (!this.isModified('Password')) return next();
+
+	const password = this.Password;
+	const hashPassword = bcrypt.hashSync(password, 10);
+	this.Password = hashPassword;
+	next();
 });
 
 const USER = mongoose.model('User', singupSchema);
